ceba9d55a143173ce547c8ca5db4ae352dace744
Tyler <tylerthardy@gmail.com>
2017-04-13 16:10:12 -0500

    Thought I had freedom on my specification of i2, d2, and s2. I changed them to fit the expected outputs.

--- HackerRank30Days/Day1.cs
+++ HackerRank30Days/Day1.cs
@@ -17,9 +17,9 @@ namespace HackerRank30Days
             //////////
 
             // Declare second integer, double, and String variables.
-            int i2 = 8;
-            double d2 = 8.4601;
-            string s2 = "seems like a useful website for career development. ~Abraham Lincoln";
+            int i2 = 12;
+            double d2 = 4.0;
+            string s2 = "is the best place to learn and practice coding!";
 
             // Read and save an integer, double, and String to your variables.
             int i3 = Int32.Parse(Console.ReadLine());
@@ -30,7 +30,7 @@ namespace HackerRank30Days
             Console.WriteLine(i + i2);
 
             // Print the sum of the double variables on a new line.
-            Console.WriteLine(Math.Round(d + d2, 1));
+            Console.WriteLine("{0:0.0}", d + d2);
 
             // Concatenate and print the String variables on a new line
             // The 's' variable above should be printed first.
